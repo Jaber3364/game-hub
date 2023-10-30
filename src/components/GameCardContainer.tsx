3b5343f284f7598e82df318c70dac83c428894ce
MartixCoder <martixcoder64@gmail.com>
2023-10-30 18:03:40 +0600

    Fatch Game Details

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -7,7 +7,12 @@ interface Props {
 
 const GameCardContainer = ({children}: Props) => {
   return (
-    <Box borderRadius={10} overflow={"hidden"}>
+    <Box
+      _hover={{transform: "scale(1.03)", transformBox: 'transform 0.15s ease-in'}}
+      borderRadius={10}
+      overflow={"hidden"}
+      marginTop={2}
+    >
       {children}
     </Box>
   );
