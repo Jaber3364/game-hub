f85199089a45aeabd22d36472949e5622b88a1ed
MartixCoder <martixcoder64@gmail.com>
2023-10-31 19:01:00 +0600

    build expandable text

--- src/Pages/GameDetailPage.tsx
+++ src/Pages/GameDetailPage.tsx
@@ -1,6 +1,7 @@
 import { useParams } from 'react-router-dom';
 import useGame from './useGame';
-import { Heading, Spinner,Text } from '@chakra-ui/react';
+import { Box, Heading, Spinner } from '@chakra-ui/react';
+import ExpandableText from '../components/ExpandableText';
 
 const GameDetailPage = () => {
   const { slug } = useParams();
@@ -10,10 +11,10 @@ const GameDetailPage = () => {
   if (error) throw error;
 
   return (
-    <>
+    <Box padding={7}>
       <Heading>{game?.name}</Heading>
-      <Text>{ game?.description_raw }</Text>
-    </>
+      <ExpandableText>{ game?.description_raw }</ExpandableText>
+    </Box>
   )
 }
 
--- src/Pages/Layout.tsx
+++ src/Pages/Layout.tsx
@@ -6,7 +6,7 @@ const Layout = () => {
   return (
     <>
       <NavBar />
-      <Box padding={7}>
+      <Box>
         <Outlet />
       </Box>
     </>
